5c83f3894f26486fb3a3fc6cd81ba89625b2b577
Joao <joaomarcio.ribeiro@gmail.com>
2021-04-28 21:15:20 -0300

    Add files via upload

--- chars.py
+++ chars.py
@@ -1,16 +1,35 @@
+<<<<<<< HEAD
+from charClasses import *
+def classSelection(chosenClass):
+    chosenClass = 0 
+    if charClass == 1:
+        import charClasses
+        chosenClass = 1
+
 def selectClass(charClass):
+
+=======
+def selectClass(charClass):
+>>>>>>> 76569999d26e19017b8ad87c20e4f38e9b0dc687
     baseHP = 25
     baseMP = 0
     baseAttack = 10
     baseDefense = 8
+<<<<<<< HEAD
+=======
     fighterHP = 30
     fighterMP = 0
     fighterAttack = 18
     fighterDefense = 13
+>>>>>>> 76569999d26e19017b8ad87c20e4f38e9b0dc687
     charHP = 0
     charMP = 0
     charAttack = 0
     charDefense = 0
+<<<<<<< HEAD
+    charClass = 0
+=======
+>>>>>>> 76569999d26e19017b8ad87c20e4f38e9b0dc687
     baseStats = """
     All classes have:
     25 HP
@@ -30,6 +49,12 @@ def selectClass(charClass):
     {baseAttack+8} Attack 
     {baseDefense+5} Defense    
     """
+<<<<<<< HEAD
+# charClass = int(input('Select your class\n[1] Fighter\n> ')
+# selectClass(charClass)
+charClass = 1
+classSelection(chosenClass)
+=======
     if userInput == 1:
         print(descFighter)
 userInput = int(input('Select your class\n[1] Fighter\n> '))
@@ -42,3 +67,4 @@ if userInput == 1:
     selectClass(1)
     print(f'Class: Fighter\n\nStats:\HP: {fighterHP}\nMP: {fighterMP}\nAttack: {fighterAttack}\nDefense: {fighterDefense}')
     satisfied = int(input('Are you satisfied?\n[1] Yes\n[2] No\n> '))
+>>>>>>> 76569999d26e19017b8ad87c20e4f38e9b0dc687
---
+++ selectClass.py
@@ -0,0 +1,4 @@
+from charClasses import *
+x = 1
+if x == 1:
+    classFighter()
