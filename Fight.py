9dd0a1659cbff551963035fce3bb9903429abb17
Joao <joaomarcio.ribeiro@gmail.com>
2021-05-03 19:10:42 -0300

    Update Fight.py

--- Fight.py
+++ Fight.py
@@ -1,18 +1,34 @@
+# Micro RPG text-based game
+# Import section
+import random
+import time
+import sys
 from openpyxl import load_workbook
 import random
 from time import sleep
 from utils import _cls
 
+# Variables
+charStrenght = 0
+charSpeed = 0
+monsterStrenght = 0
+monsterSpeed = 0
+charHP = 0
+monsterHP = 0
+charAttack = 0
+monsterAttack = 0
+
 EnemyChosen = random.randint(1, 3)
 book = load_workbook(filename="/home/joao/00-Dev/Python/Secret-World-RPG/data.xlsx")
 UserStats = book["UserStats"]
-EnemyClass = book["EnemyClass"]
-EnemyStats = book["EnemyStats"]
 UserClass = UserStats['A2'].value
 UserHP = UserStats['B2'].value
 UserMP = UserStats['C2'].value
 UserAttack = UserStats['D2'].value
 UserDefense = UserStats['E2'].value
+EnemyClass = book["EnemyClass"]
+EnemyStats = book["EnemyStats"]
+
 if EnemyChosen == 1:
     EnemyClass = EnemyStats['A2'].value
     EnemyHP = EnemyStats['B2'].value
@@ -30,45 +46,61 @@ else:
     EnemyHP = EnemyStats['B4'].value
     EnemyMP = EnemyStats['C4'].value
     EnemyAttack = EnemyStats['D4'].value
-    EnemyDefense = EnemyStats['E4'].value
-def Actions():
-    _UserHP = 1
-    _EnemyHP = 1
-    while _UserHP or _EnemyHP <= 0:
-        print('[1] Attack\n[2] Defend\n[3] Spell\n[4] Run')
-        action = int(input('Select an action:\n> '))
-        if action > 4:
-            print('Invalid option, try again')
-            sleep(1)
-            _cls()
-            return Actions()
-        elif action == 1:
-            _EnemyDefense = random.randint(1, EnemyDefense)
-            _UserAttack = random.randint(1, UserAttack)
-            if _EnemyDefense > _UserAttack:
-                print(f'You attack {EnemyClass} with {_UserAttack} power, {EnemyClass} defended with {_EnemyDefense}')
-            elif _EnemyDefense < _UserAttack:
-                _EnemyHP = EnemyHP - _UserAttack
-                print(f'You hit {EnemyClass} with {_UserAttack} power!\n{EnemyClass} HP: {_EnemyHP}')
-    
-                if _EnemyHP <= 0:
-                    print('Enemy defeated!')
-                    break
-            else:
-                print(f'{UserClass} attack and {EnemyClass} defense is the same, nothing happened')
-        if action == 2:
-            _EnemyAttack = random.randint(1, EnemyAttack)
-            _UserDefense = random.randint(1, UserDefense)
-            if _UserDefense > _EnemyAttack:
-                print(f'You defend the attack with {_UserDefense}')
-            elif _UserDefense < _UserAttack:
-                _UserHP = UserHP - _EnemyAttack
-                print(f'You defended {EnemyClass} with {_UserDefense}, but {EnemyClass} hit you with {EnemyAttack}!\nYour HP: {_UserHP}')
-                if _UserHP <= 0:
-                    print('You died!')
-                    break
-            else:
-                print(f'{UserClass} defense and {EnemyClass} attack is the same, nothing happened')
-    
-print(f'A wild {EnemyClass} appears!\nHP: {EnemyHP}\nMP: {EnemyMP}\nAttack: {EnemyAttack}\nDefense: {EnemyDefense}\nWhat you will do?')
-Actions()
+    EnemyDefense = EnemyStats['E4'].value   
+
+def fight(charStrenght, charSpeed,monsterStrenght,monsterSpeed):
+	charHP = UserHP
+	monsterHP = EnemyHP
+	charPower = 0
+	charHits = 0
+	avgCharPower = 0
+	monsterHits = 0
+	monsterPower = 0
+	avgMonsterPower = 0
+	while UserHP or EnemyHP <= 0:
+		charAttack = random.randint(1, UserAttack)
+		charSpeed = random.randint(1, UserDefense)
+		monsterAttack = random.randint(1, EnemyAttack)
+		monsterSpeed = random.randint(1,EnemyDefense)
+        
+		if monsterAttack == charAttack:
+			print(f'{UserClass} and {EnemyClass} landed same attack power.\nNothing happened.')
+			time.sleep(0.5)
+			
+		if charSpeed > monsterSpeed and charAttack > monsterAttack:
+			charHits = charHits + 1
+			charPower = charPower + charAttack
+			monsterHP = monsterHP - charAttack
+			print(f'You hit the monster with {charAttack}.\nMonster have {monsterHP} HP',)
+			time.sleep(0.5)
+			if monsterHP <= 0:
+				avgCharPower = charPower / charHits
+				avgCP = "{:.2f}".format(avgCharPower)
+				print(f'Congratulations! You win the battle\nYou landed {charHits} hits.\nAverage power {avgCP}')
+				prompt = str(input('Do you want to play again? (Y/N)')).lower()
+				if prompt == 'y':
+					fight(charStrenght, charSpeed,monsterStrenght,monsterSpeed)
+				else:
+					break
+		elif charSpeed < monsterSpeed and charAttack < monsterAttack:
+			monsterHits = monsterHits + 1
+			monsterPower = monsterPower + monsterAttack
+			charHP = charHP - monsterAttack
+			print(f'You try to hit {EnemyClass} with {charAttack}, but {EnemyClass} strikes you back with {monsterAttack}\nYou have {charHP} HP')
+			time.sleep(0.5)
+			if charHP <= 0:
+				avgMonsterPower = monsterPower / monsterHits
+				avgMP = "{:.2f}".format(avgMonsterPower)
+				print(f'{UserClass} you lost the battle!\n{EnemyClass} landed {monsterHits} hits.\nAverage power {avgMP}')
+				prompt = str(input('Do you want to play again? (Y/N) ')).lower()
+				if prompt == 'y':
+					fight(charStrenght, charSpeed,monsterStrenght,monsterSpeed)
+				else:
+					break
+		if charSpeed > monsterSpeed and charAttack < monsterAttack:
+			print(f'{EnemyClass} miss the attack!')
+			time.sleep(0.5)
+		elif charSpeed < monsterSpeed and charAttack > monsterAttack:
+			print(f'{UserClass} miss the attack!')
+			time.sleep(0.5)
+fight(charStrenght, charSpeed,monsterStrenght,monsterSpeed)
